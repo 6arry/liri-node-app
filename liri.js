179f70f2f16ca0fd43aafbd3ea9f0320b9ae688a
gccunanan <gccunanan@gmail.com>
2019-06-18 00:48:41 -0700

    updated with axios getBandInTown function:working

--- liri.js
+++ liri.js
@@ -6,10 +6,64 @@ var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-  console.log(data.tracks.items[0]); 
+var command = process.argv[2];
+var userInput = process.argv[3]
+
+const axios = require('axios');
+
+axios.get('/user?ID=12345')
+  .then(function (data) {
+    // handle success
+    console.log(data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
   });
+
+var getBandInTown = function(bandName){
+    axios.get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp")  
+    .then(function(data){
+        console.log(data);
+    })  
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+      .finally(function () {
+        // always executed
+    });
+}
+
+
+var getSpotifySong = function(songName){
+    spotify.search({ type: 'track', query: 'songName' }, function(err, data) {
+        if (err) {
+        return console.log('Error occurred: ' + err);
+        }
+    
+    console.log(data.tracks.items[0]); 
+    });
+}
+
+var pick = function(caseCommand, functionData){
+    switch(caseCommand){
+        case 'concert-this':
+            getBandInTown(functionData); 
+            break;
+        case 'spotify-this-song':
+            getSpotifySong(functionData);
+            break;
+        default:
+        console.log('LIRI has now idea... what do you want???')
+    }
+}
+
+var runThis = function(firstArg, secondArg){
+    pick(firstArg, secondArg);
+};
+
+runThis(command, userInput);
