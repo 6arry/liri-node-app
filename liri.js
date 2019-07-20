cd88bba30a1018777e43473da263d65406995704
gccunanan <gccunanan@gmail.com>
2019-07-20 02:13:49 -0700

    forgot to require fs npm: added to liri.js & package.json

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var moment = require('moment');
+var fs = require('fs');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
